242ccf11db5434e836fa653d6a147c596d39ee43
Arunkumar Ravichandran <arunkumar.ravichandran@lvchn.com>
2019-07-16 14:28:56 +0530

    React-Simple-Chatbot initial stage

---
+++ UI/src/components/SimpleChatBot/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ChatBot from 'react-simple-chatbot';
+
+class SimpleChatBot extends Component {
+    render() {
+        return (
+            <div id="simpleChatBot">
+                <ChatBot/ >
+            </div>
+        )
+    }
+}
+
+export default SimpleChatBot;
